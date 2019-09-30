a7dff17b91a7028396a5b35b7781b8837c78962e
root <tongyf1988@gmail.com>
2019-09-30 18:29:43 +0800

    test

---
+++ shadowsocks/test.py
@@ -0,0 +1,15 @@
+#-*-coding:utf-8-*-
+import redis
+
+def insert(ip):
+    client = redis.Redis(host='127.0.0.1', port=6379, db=0)
+    ip_list = []
+    ip_list = eval(client.get('trust_ip_list'))
+    if ip not in ip_list:
+        ip_list.append(ip)
+    client.set('trust_ip_list', str(ip_list))
+
+if __name__ == '__main__':
+
+    ip = ''
+    insert(ip)
